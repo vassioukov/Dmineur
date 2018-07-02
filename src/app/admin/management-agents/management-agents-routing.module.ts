64ffbd5b3d7c0589629fbaeacac3d15b2e8de50e
vassioukov <vincent.vallet@me.com>
2018-07-02 18:02:01 +0200

    ajout creation conseiller

--- src/app/admin/management-agents/management-agents-routing.module.ts
+++ src/app/admin/management-agents/management-agents-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ManagementAgentsComponent } from './management-agents.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
 const routes: Routes = [
-	{
-		path:'',
-		component: ManagementAgentsComponent
-	}	
+
+    {
+        path:'',  component: ManagementAgentsComponent,
+        children: [
+            { path: 'new-user', component: NewUserComponent },
+        ]
+    }
 ];
 
 @NgModule({
