c775130a075e6305fc7353ca069c86559ada0e58
InesBoudriga <inesboudriga1@gmail.com>
2018-07-04 13:57:53 +0200

    merging ...

---
+++ src/app/admin/management-agents/new-user/new-user.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../services/user.service';
+
+import { Router } from '@angular/router';
+import { User } from '../models/users.modele';
+
+@Component({
+  selector: 'app-new-user',
+  templateUrl: './new-user.component.html',
+  styleUrls: ['./new-user.component.scss']
+})
+export class NewUserComponent implements OnInit {
+
+  userForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private userService: UserService,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.userForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      matricule: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telephone: ['',[Validators.required, Validators.pattern('[0-9]+')]],
+    });
+  }
+
+  onSubmitForm() {
+    const formValue = this.userForm.value;
+    const newUser = new User(
+      formValue['firstName'],
+      formValue['lastName'],
+      formValue['matricule'],
+      formValue['email'],
+      formValue['telephone']
+    );
+    this.userService.addUser(newUser);
+    this.router.navigate(['/admin/managementAgents']);
+  }
+
+}
